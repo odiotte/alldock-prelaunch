b39a642d7acac3341f89f37dfb4ff3acb1fff3ec
Olivier Diotte <olivier.diotte@gmail.com>
2015-02-23 11:12:47 -0500

    initialized secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Prelaunchr::Application.config.secret_token = ENV["RAILS_SECRET"] || 'xxxxxxxx-change-me-please-xxxxxxxx'
+Prelaunchr::Application.config.secret_token = ENV["RAILS_SECRET"] || '63115e45b3abf91eaf95b44abf19fa48d868ab1bc647b2d75f8aa2e91f48a3244b14f780d34ff64dac4e4e8e3c4ab9be2d170a6e010e0767115bc198780ff9b1'
